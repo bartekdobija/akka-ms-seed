3bb7cca06da6899f9ddf9f0d591693bd59e1f79c
Bartek Dobija <bartek.dobija@gmail.com>
2017-11-14 21:40:49 +0100

    kafka client libs

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 // This build is for this Giter8 template.
 // To test the template run `g8` or `g8Test` from the sbt session.
 // See http://www.foundweekends.org/giter8/testing.html#Using+the+Giter8Plugin for more details.
-lazy val akkamsseed = (project in file(".")).
+lazy val `akka-ms-seed` = (project in file(".")).
   settings(
     test in Test := {
       val _ = (g8Test in Test).toTask("").value
--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -6,9 +6,12 @@ object Dependencies {
   lazy val akkaHttpVersion = "10.0.10"
   lazy val scalajHttpVersion = "2.3.0"
   lazy val dropwizardVersion  = "1.2.0"
+  lazy val kafkaVersion = "0.11.0.1"
 
   val akkaService = Seq(
     "org.scalatest" %% "scalatest" % scalaTestVersion % Test,
+
+    "org.apache.kafka" % "kafka-clients" % kafkaVersion,
 
     "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
     "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
